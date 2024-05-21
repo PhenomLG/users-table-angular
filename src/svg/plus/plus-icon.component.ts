43f30df86d1f2daa6ca6a9ec36e9319ae4426350
Svyatoslav <svyatoslav.anufriev@sensei.plus>
2024-05-21 18:21:55 +0300

    Added svg

---
+++ src/svg/plus/plus-icon.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'initium-plus-icon',
+    standalone: true,
+    imports: [],
+    templateUrl: './plus-icon.component.svg',
+    styleUrl: './plus-icon.component.scss',
+    host: {
+        width: "20",
+        height: "20",
+        viewBox: "0 0 20 20",
+        fill: "none",
+        xmlns: "http://www.w3.org/2000/svg"
+    }
+
+})
+export class PlusIconComponent {
+
+}
---
+++ src/svg/trash/trash.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'initium-trash',
+    standalone: true,
+    imports: [],
+    templateUrl: './trash.component.svg',
+    styleUrl: './trash.component.scss',
+    host: {
+        width: "36",
+        height: "36",
+        viewBox: "0 0 36 36",
+        fill: "none",
+        xmlns: "http://www.w3.org/2000/svg"
+    }
+})
+export class TrashComponent {
+
+}
